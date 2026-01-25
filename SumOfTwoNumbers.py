0572f056e670cc8573fb1bc0c4aea73b8b618ec6
Amshar <20dpcs004hy@manuu.edu.in>
2026-01-25 16:07:51 +0530

    int to float

--- SumOfTwoNumbers.py
+++ SumOfTwoNumbers.py
@@ -1,3 +1,3 @@
-num1 = int(input("Enter 1st Number : "))
-num2 = int(input("Enter 2nd Number : "))
+num1 = float(input("Enter 1st Number : "))
+num2 = float(input("Enter 2nd Number : "))
 print(f"Sum of your number is : {num1+num2}")
