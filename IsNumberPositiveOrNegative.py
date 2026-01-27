ead1d758be878f67f5d2e9ee700eea26ccff85c6
Amshar <20dpcs004hy@manuu.edu.in>
2026-01-27 18:51:48 +0530

    +ve-vem, leap year, odd even code added

---
+++ IsNumberPositiveOrNegative.py
@@ -0,0 +1,5 @@
+num = float(input("Enter Number to check +ve or -ve : "))
+if num < 0 :
+    print("Number is Negative")
+else:
+    print("Number is Positive")
---
+++ oddEven.py
@@ -0,0 +1,5 @@
+num = int(input("Enter Number to find odd or even : "))
+if num % 2 == 0 :
+    print("Given number is Even")
+else :
+    print("Given number is odd")
